b39f12a258051a4e8f4beda95527995c6579dae4
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2022-09-06 02:58:21 +0300

    İlk Gönderim

---
+++ app/src/main/java/com/tahacinar/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.tahacinar.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val logo: ImageView = findViewById(R.id.logo)
+
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+
+        }
+    }
+}
